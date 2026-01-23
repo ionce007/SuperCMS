6817c09817211ffb60c2545e5bfa3a773d1ad316
ionce007 <ionce@163.com>
2026-01-23 08:50:38 +0800

    app.js添加调试信息。

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 ﻿const Chanjs = require("chanjs");
 const chan = new Chanjs();
 //global.voiceData = getVoiceData();
-
+console.log('Chan.config = ', Chan.config);
 chan.start();
 chan.run((port) => {
   console.log(`SuperCMS 运行于 ${port} 端口，可访问： http://localhost:${port}`);
