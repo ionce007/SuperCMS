997057a96a4bc4a5c856bf56213c981272a56c13
ionce007 <ionce@163.com>
2026-01-24 19:29:24 +0800

    调试信息2

--- app/modules/api/service/stock.js
+++ app/modules/api/service/stock.js
@@ -1119,6 +1119,7 @@ class StockService extends BaseService {
                     "Referer": "https://emdata.eastmoney.com/",
                 },
             });
+            console.log('data = ',data);
             return data;
         }
         catch (err) {
@@ -1447,13 +1448,15 @@ class StockService extends BaseService {
         try {
             let tradeDates = await this.getWebTradeDate();
             const limitDates = await this.limitUpDown_TradeDate();
-            let isNewDate = utils.formatDate(new Date(), 'yyyy-MM-dd');
+            let isNewDate = utils.dateFormat(new Date(), 'yyyy-MM-dd');
             if (!limitDates.result) isNewDate = tradeDates.result.data[0].DATE
             else isNewDate = limitDates.result.data.filter(item => item.IS_NEW === '1')[0].TRADE_DATE;
             const endDate = new Date(new Date(isNewDate).setFullYear(new Date(isNewDate).getFullYear() - 1));
             let data = [];
             let calendar = [];
+            
             console.log('tradeDates = ', tradeDates);
+
             for (let i = 0; i < tradeDates.result.data.length; i++) {
                 if (new Date(tradeDates.result.data[i].DATE) >= endDate) {
                     let is_new = 0;
